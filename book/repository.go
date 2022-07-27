7a62afcc0ebcd3ef65b9ce7a55dff5f6298b5c36
ilham fathullah <ilhamfathullah04@gmail.com>
2022-07-27 14:10:15 +0700

    Golang

---
+++ book/repository.go
@@ -0,0 +1,43 @@
+package book
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	GetBooks() ([]Book, error)
+	GetBook(ID int) (Book, error)
+	AddBook(book Book) (Book, error)
+	UpdateBook(ID int) (Book, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+// func nama-func(paramas) nilai-yg-dikembalikan
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db} // agar bisa diakses di main
+}
+
+// func (struct) nama-func(paramas) nilai-yg-dikembalikan
+func (r *repository) GetBooks() ([]Book, error) {
+	var books []Book
+	err := r.db.Find(&books).Error
+	return books, err
+}
+
+func (r *repository) GetBook(ID int) (Book, error) {
+	var book Book
+	err := r.db.Find(&book, ID).Error
+	return book, err
+}
+
+func (r *repository) AddBook(book Book) (Book, error) {
+	err := r.db.Create(&book).Error
+	return book, err
+}
+
+func (r *repository) UpdateBook(ID int) (Book, error) {
+	var book Book
+	err := r.db.Find(&book, ID).Error
+	return book, err
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"golang-api/book"
 	"golang-api/handler"
 	"log"
@@ -22,99 +21,39 @@ func main() {
 	}
 
 	db.AutoMigrate(&book.Book{}) // migration
-	fmt.Println("Databace connect")
 
-	// ================== RAW QUERY ====================
-	// ################## INSERT ##################
-	// err = db.Exec("INSERT INTO books(title,description,price,created_at,updated_at) VALUES ('Manusia Hariamu','Test',2000,?,?)", time.Now(), time.Now()).Error
-	// ############################################
+	bookRepository := book.NewRepository(db)
 
-	// ############# SELECT SINGLE ##############
-	// var book book.Book
-	// db.Raw("SELECT * FROM books WHERE id=?", 4).Scan(&book)
-	// fmt.Printf("Data : %v", book)
-	// ############################################
+	// ############# GET BOOKS ###############
+	// books, err := bookRepository.GetBooks()
 
-	// ############# SELECT MULTIPLE ##############
-	// var books []book.Book
-	// db.Raw("SELECT * FROM books").Scan(&books)
-
-	// for _, b := range books {
-	// 	fmt.Printf("Data : %v", b)
+	// for _, book := range books {
+	// 	fmt.Println("Title : ", book.Title)
 	// }
-	// ############################################
+	// #######################################
 
-	// ############# UPDATE DATA ##############
-	// var book []book.Book
-	// db.Raw("UPDATE books SET title=? WHERE id=?", "Man of Body Tiger", 3).Scan(&book)
-	// ########################################
+	// ############## GET BOOK ###############
+	// book, _ := bookRepository.GetBook(7)
 
-	// ############# DELETE DATA ##############
-	// db.Exec("DELETE FROM books WHERE id=?", 6)
-	// ########################################
-	// =================================================
+	// fmt.Println("Title : ", book.Title)
+	// #######################################
 
-	// ================== ORM QUERY ====================
-	// ################### INSERT ###################
+	// ############## ADD BOOK ###############
 	// book := book.Book{
-	// 	Title:       "Manusia Hariamu",
-	// 	Description: "Buku tentang seorang manusia yang menjadi harimau",
-	// 	Price:       200000,
-	// }
-	// err = db.Create(&book).Error
-	// ###############################################
-
-	// ############# SELECT SINGLE DATA #############
-	// var book book.Book // store single data
-	// err = db.Take(&book, 4).Error
-	// if err != nil {
-	// 	fmt.Println("================================")
-	// 	fmt.Println("|| Error creating book record ||")
-	// 	fmt.Println("================================")
-	// }
-	// fmt.Println("Title : ", book.Title) //single data
-	// fmt.Printf("Data : %v", book)       //single data
-	// ###############################################
-
-	// ############ SELECT MULTIPLE DATA #############
-	// var books []book.Book // store multiple data
-	// err = db.Find(&books).Error
-	// if err != nil {
-	// 	fmt.Println("================================")
-	// 	fmt.Println("|| Error creating book record ||")
-	// 	fmt.Println("================================")
+	// 	Title:       "Who Came After Die?",
+	// 	Description: "Awesome book",
+	// 	Price:       90000,
 	// }
 
-	// for _, b := range books {
-	// 	fmt.Printf("Data : %v", b) //multiple data
-	// }
-	// ###############################################
+	// bookRepository.AddBook(book)
 
-	// ################ UPDATE DATA ##################
-	// var book book.Book
+	// fmt.Println("Title : ", book.Title)
+	// #######################################
 
-	// err = db.Take(&book, 3).Error
-	// if err != nil {
-	// 	fmt.Println("================================")
-	// 	fmt.Println("|| Error creating book record ||")
-	// 	fmt.Println("================================")
-	// }
+	// ############## ADD BOOK ###############
+	book, _ := bookRepository.GetBook(8)
 
-	// book.Title = "Man Tiger Wong"
-	// err = db.Save(&book).Error
-	// if err != nil {
-	// 	fmt.Println("================================")
-	// 	fmt.Println("|| Error creating book record ||")
-	// 	fmt.Println("================================")
-	// }
-	// ###############################################
-
-	// ################ DELETE DATA ##################
-	// var book book.Book
-	// db.Take(&book, 3)
-
-	// db.Delete(&book)
-	// ###############################################
+	// #######################################
 
 	router := gin.Default()
 
